8937087b9fc92875c71d651a5f996ebf4230999e
wangyuxiao <wyx03140827ym@163.com>
2021-09-24 15:38:36 +0800

    fix: 提交地图

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="screen-box">
-
+    <mapDot></mapDot>  
   </div>
 </template>
 <script>
-
+import mapDot from '../components/map_dot.vue'
+export default {
+  components: {
+    mapDot
+  }
+}
 </script>
 <style lang="scss" scoped>
 .screen-box {
   width: 100%;
   height: 100%;
-  // background-image: url(../assets/img/bg.png);
+  background-image: url(../assets/img/bg.png);
   background-size: 100% 100%;
 }
 </style>

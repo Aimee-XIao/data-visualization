e85dc2a0cede91297a2c4c19db3542b5208b9bee
wangyuxiao <wyx03140827ym@163.com>
2021-09-24 08:57:34 +0800

    fix：提交图片

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,4 +1,16 @@
 <template>
-  <div></div>
+  <div class="screen-box">
+
+  </div>
 </template>
 <script>
+
+</script>
+<style lang="scss" scoped>
+.screen-box {
+  width: 100%;
+  height: 100%;
+  background-image: url(../assets/img/bg.png);
+  background-size: 100% 100%;
+}
+</style>

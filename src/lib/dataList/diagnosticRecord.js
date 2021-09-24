18d1a559f1574a8bbfd7cd3f7f497f7aac0bf5b6
chenjuan <872369200@qq.com>
2021-09-24 09:31:45 +0800

    添加lib

---
+++ src/lib/dataList/diagnosticRecord.js
@@ -0,0 +1,173 @@
+/**
+@description: 诊断记录
+编号  id
+单位  unit
+药品类别 drugType
+药品名称  drugName
+数量  nnumber
+给药方式  doseType
+是否皮试 isSkinTest
+开单科室  BillingDepartment
+患者姓名  patientName
+医生嘱托  DoctorAdvice
+患者编号  patientNo
+就诊IDs   visitIDS
+*/
+ 
+export const diagnosticRecord = [
+    {
+      id: 125445,
+      unit: '西京医院',
+      drugType: '西药',
+      drugName: '阿莫西林',
+      nnumber: 3,
+      doseType: 2,
+      isSkinTest: N,
+      BillingDepartment: '内科',
+      patientName: '张三',
+      DoctorAdvice: 'wu',
+      patientNo: 1254861,
+      visitIDS: 1254841
+    },
+    {
+        id: 125446,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 125447,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 125448,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 125449,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254410,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254411,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254412,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254413,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254414,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    },
+    {
+        id: 1254415,
+        unit: '西京医院',
+        drugType: '西药',
+        drugName: '阿莫西林',
+        nnumber: 3,
+        doseType: 2,
+        isSkinTest: N,
+        BillingDepartment: '内科',
+        patientName: '张三',
+        DoctorAdvice: 'wu',
+        patientNo: 1254861,
+        visitIDS: 1254841
+    }
+    
+]
---
+++ src/lib/util.js
@@ -0,0 +1,44 @@
+import { prescriptionDetails } from './dataList/prescriptionDetails';
+import { diagnosticRecord } from './dataList/diagnosticRecord';
+import { visitRecord } from './dataList/visitRecord';
+
+
+export const utilFun = {
+   /**
+  * @description:  求和
+  * @param {Sting} keyName 求和列字段名
+  * @param {Sting} fitlerNmae 过滤字段名
+  * @param {Sting} val 过滤值
+  * @param {Array} obj 数据
+  * @param {boolean} isSet 是否去重
+  * @return {number} 合计值
+  */
+    addNum ({keyName, fitlerNmae, val, obj= [], isSet = false}) {
+        let arr =  isSet ? new Set(obj)  : obj        
+        let $total = 0
+        $total = this.fitlerList(fitlerNmae, val, obj).reduce(function (total, currentValue){
+            return currentValue[keyName] ? (total + currentValue[keyName]) : total;
+        }, 0)
+        return $total    
+    },
+    /**
+     * @description:  过滤方法
+     * @param {Sting} keyName 过滤列字段名
+     * @param {Sting} val 过滤值
+     * @param {any} obj 数据
+     * @return {Array}} 滤过后数组对象
+     */
+    fitlerList ({keyName = 'unit', val= 'NKG', obj}) {
+        return obj.fitler(item => item[keyName] == val )
+    },
+
+    /**
+      * @description: 总览数据: 医生总量、接诊次数、接诊人数、用药总量
+      */
+     overviewData(value) {
+      let dorNum = this.addNum('dataNum', 'unit', value, this.visitRecord)
+      let timesNum = this.addNum('dataNum', 'unit', value, this.visitRecord)
+      let personNm = this.addNum('dataNum', 'unit', value, this.visitRecord)
+      let drugNum = this.addNum('dataNum', 'unit', value, this.diagnosticRecord) 
+    }
+}
